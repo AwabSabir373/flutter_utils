9987789f28b524e6db741e59e6823abe76008adc
awab <awab.sabir@broadvisions.net>
2023-08-02 22:57:23 +0500

    material 3 bottom sheet added

--- lib/dialogs/dialogs.dart
+++ lib/dialogs/dialogs.dart
@@ -22,17 +22,28 @@ showFullWidthDialog(
       ),
     );
   } else {
+    var userMaterial3 = Theme.of(AppCntx.currentContext).useMaterial3;
     showModalBottomSheet(
       backgroundColor: Colors.transparent,
-      elevation: elevation,
       context: context??AppCntx.currentContext,
       isDismissible: isCancelable,
       isScrollControlled: true,
       useSafeArea: true,
       builder: (context) => FractionallySizedBox(
         heightFactor: heightFactor,
-        child: child,
+        child: userMaterial3? _material3sheet(child, elevation): child,
       ),
     );
   }
+}
+
+Widget _material3sheet(Widget child, double elevation){
+  var colorScheme = Theme.of(AppCntx.currentContext).colorScheme;
+  return Material(
+    color: colorScheme.surface,
+    shadowColor: colorScheme.onSurface,
+    surfaceTintColor: colorScheme.onSurface,
+    elevation: elevation,
+    child: child,
+  );
 }
