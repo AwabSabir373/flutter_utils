85d77a61efdf4ebeed6bfc66913b5fcd485abbbf
awab <awab.sabir@broadvisions.net>
2023-08-16 11:03:22 +0500

    connectivity check added

--- lib/custom_utils/custom_utils.dart
+++ lib/custom_utils/custom_utils.dart
@@ -1,5 +1,6 @@
 import 'dart:developer' as developer;
 
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 void logMessage(message) {
@@ -40,14 +41,14 @@ void scrollDirectionHandler({required ValueChanged<bool> function, required Scro
   }
 }
 
-//check internet connection
-// Future<bool> checkInternetConnection() async {
-//   var connectivityResult = await (Connectivity().checkConnectivity());
-//   if (connectivityResult == ConnectivityResult.mobile) {
-//     return true;
-//   } else if (connectivityResult == ConnectivityResult.wifi) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
+// check internet connection
+Future<bool> checkInternetConnection() async {
+  var connectivityResult = await (Connectivity().checkConnectivity());
+  if (connectivityResult == ConnectivityResult.mobile) {
+    return true;
+  } else if (connectivityResult == ConnectivityResult.wifi) {
+    return true;
+  } else {
+    return false;
+  }
+}
